bc5e3ba54eb98c1c04cebc4ec8e5149f65c5e6c8
laurys15951 <laurys.strav@gmail.com>
2024-07-23 11:50:08 +0300

    Updated player
    moved player script to player scene and updated player movement logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,31 @@
 extends CharacterBody2D
 
+@export var max_speed = 400
+@export var accel = 1500
+@export var friction = 600
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var input = Vector2.ZERO
 
 func _physics_process(delta):
-	# Add the gravity.
-	# if not is_on_floor():
-	# 	velocity.y += gravity * delta
+	player_movement(delta)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length()> (friction*delta):
+			velocity -= velocity.normalized()*(friction*delta)	#stopping or decelerating
+		else:
+			velocity = Vector2.ZERO		#full stop
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity+=(input*accel*delta)	#speed up
+		velocity = velocity.limit_length(max_speed)	#limit speed
+	
 	move_and_slide()
+
+
