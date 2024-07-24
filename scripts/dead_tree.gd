8fca95b71653143e18aa31c8a42b5ec379959daa
laurys15951 <laurys.strav@gmail.com>
2024-07-24 20:25:50 +0300

    Needs a check if works

--- scripts/dead_tree.gd
+++ scripts/dead_tree.gd
@@ -1,20 +1,34 @@
 extends Node2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
-var max_branches = 6
+@export var max_branches = 6
 var branches_dropped = 0
+
+var branch = preload("res://scenes/branch_collectable.tscn")
 
 
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
-	if branches_dropped<max_branches:
+	if branches_dropped < max_branches:
 		$branch_fall_timer.start()
 
 func _process(delta):
 	pass
 
 func _on_branch_fall_timer_timeout():
-	pass								#spawn dropable branch
+	drop_branch()
 
 func _on_interact():
 	self.visible = false
+
+func drop_branch():
+	branches_dropped += 1
+	var branch_instance = branch.instantiate()
+	branch_instance.global_position = $Marker2D.global_position
+	get_parent().add_child(branch_instance)
+	
+	await  get_tree().create_timer(3).timeout
+	
+	if branches_dropped < max_branches:
+		$branch_fall_timer.start()
+	
