c05cde2c0ea5ede0bd6477ca7193d5b001fd90a4
laurys15951 <laurys.strav@gmail.com>
2024-07-27 23:16:53 +0300

    Pebbels drop in 1 of 6 random forms

--- scripts/pebble_collectable.gd
+++ scripts/pebble_collectable.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
-@onready var sprite_2d = $Sprite2D
 @onready var player = get_tree().get_first_node_in_group("player")
 
 @export var item: InvItem
 
 
 func _ready():
+	$AnimatedSprite2D.set_frame(randi_range(0,6))
 	interaction_area.interact = Callable(self, "_collect")
 
 func _collect():
