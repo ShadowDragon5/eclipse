0a4cc3d884dea25b87f7751d38e57ecbdb85923d
laurys15951 <laurys.strav@gmail.com>
2024-07-30 00:33:34 +0300

    Resources work
    Resource spawning works, further ranges need to be added and set

--- scripts/ResourceSpawner.gd
+++ scripts/ResourceSpawner.gd
@@ -4,19 +4,19 @@ extends Node2D
 @export var spawn_radius_end = 10
 @export var spawn_angle_start = 0
 @export var spawn_angle_end = 2*PI
-@export var branch_from = 0
-@export var branch_to = 1
-@export var tree_from = 0
-@export var tree_to = 1
-@export var forest_from = 0
-@export var forest_to = 0
-@export var pebble_from = 0
-@export var pebble_to = 1
-@export var boulder_from = 0
-@export var boulder_to = 1
+@export var branch_from : float = 0.0
+@export var branch_to : float = 1.0
+@export var tree_from : float = 0.0
+@export var tree_to : float = 1.0
+@export var forest_from : float = 0.0
+@export var forest_to : float = 0.0
+@export var pebble_from : float = 0.0
+@export var pebble_to : float = 1.0
+@export var boulder_from : float = 0.0
+@export var boulder_to : float = 1.0
 
 var branch_radius = 8
-var tree_radius = 16
+var tree_radius = 12
 var forest_radius = 64
 var pebble_radius = 8
 var boulder_radius = 16
@@ -42,11 +42,11 @@ func _process(delta):
 
 func _spawn_resource(resource, resource_radius, from_percent, to_percent):
 	var full_resource = PI*((spawn_radius_end ** 2 - spawn_radius_start ** 2) / resource_radius ** 2)
-	var resource_count = randi_range(from_percent, to_percent) * full_resource
+	var resource_count: int = randf_range(from_percent, to_percent) * full_resource
 	for i in range(0,resource_count):
-		var angle = randi_range(spawn_angle_start, spawn_angle_end)
+		var angle = randf_range(spawn_angle_start, spawn_angle_end)
 		var dist = randi_range(spawn_radius_start, spawn_radius_end)
 		var resource_instance = resource.instantiate()
-		resource_instance.global_position.x = dist * cos(angle)
-		resource_instance.global_position.y = dist * sin(angle)
+		resource_instance.global_position.x = 32 * dist * cos(angle)
+		resource_instance.global_position.y = 32 * dist * sin(angle)
 		add_child(resource_instance)
