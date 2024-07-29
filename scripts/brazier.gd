4a6ff9156f26605adea191e95e75cddaf6dea0bf
laurys15951 <laurys.strav@gmail.com>
2024-07-29 14:09:39 +0300

    player shadowing & burning
    player has shadow and burn health that decreases, as well as light area shrinks

--- scripts/brazier.gd
+++ scripts/brazier.gd
@@ -8,15 +8,17 @@ extends StaticBody2D
 
 var fuel_time = 30
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	interaction_area.interact = Callable(self,"_add_fuel")
 	burn_time.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	#print(delta)
+	var left_time = burn_time.get_time_left() *0.5
+	$PointLight2D.scale.x = left_time/10
+	$PointLight2D.scale.y = left_time/20
+	$LightArea.scale.x = left_time/5
+	$LightArea.scale.y = left_time/10
+
 
 func _add_fuel():
 	if player.has_item(accaptable_fuel):
@@ -25,7 +27,14 @@ func _add_fuel():
 	else:
 		print("no fuel in inventory")
 	print(burn_time.get_time_left())
-	
 
 func _on_timer_timeout():
 	print ("Game Over")
+
+func _on_light_area_body_entered(body):
+	if body.has_method("player"):
+		body.entered_light_area()
+
+func _on_light_area_body_exited(body):
+	if body.has_method("player"):
+		body.exit_light_area()
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,10 +6,41 @@ extends CharacterBody2D
 
 @export var inv: Inv
 
+var max_burn_health = 15
+var max_shadow_health = 25
+var burn_rate = 2
+
+var is_area_light = 0
+var burn_health = 15
+var shadow_health = 25
+
 var input = Vector2.ZERO
+
+func _ready():
+	burn_health = max_burn_health
+	shadow_health = max_shadow_health
 
 func _physics_process(delta):
 	player_movement(delta)
+	if is_area_light > 0:
+		burn_shadow(delta)
+	else:
+		shadow_darken(delta)
+	print("health " + str(burn_health) + " shadow " + str(shadow_health))
+
+func burn_shadow(delta):
+	if shadow_health < max_shadow_health:
+		burn_health -= delta / burn_rate
+		shadow_health += delta
+
+func shadow_darken(delta):
+	shadow_health -= delta
+	if shadow_health <= 0:
+		lose_body()
+
+func lose_body():
+	print("body gone")
+	pass
 
 func get_input():
 	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
@@ -38,3 +69,12 @@ func has_item(item):
 
 func remove_item(item):
 	inv.remove_item(item)
+
+func player():
+	pass
+
+func entered_light_area():
+	is_area_light += 1
+
+func exit_light_area():
+	is_area_light -= 1
